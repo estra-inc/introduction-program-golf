e6b9553e1ea0f8287ed83a7d0c026bd901d2adab
motoki_yuto <motoki.youto293@gmail.com>
2025-03-17 11:39:30 +0900

    feat: ルーティング定義

--- backend/app/Http/Controllers/GolfCourseController.php
+++ backend/app/Http/Controllers/GolfCourseController.php
@@ -6,8 +6,19 @@ use Illuminate\Routing\Controller;
 
 class GolfCourseController extends Controller
 {
+    /**
+     * ゴルフ場一覧
+     */
     public function index()
     {
         return 'Hello World';
+    }
+
+    /**
+     * ゴルフ場詳細
+     */
+    public function show(string $golfCourseId)
+    {
+        return $golfCourseId;
     }
 }
