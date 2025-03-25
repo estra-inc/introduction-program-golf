c6eed4e08dda2b6e88cd986a46ac18cc0f453b00
motoki_yuto <motoki.youto293@gmail.com>
2025-03-26 01:47:36 +0900

    feat: Buttonコンポーネント作成

---
+++ frontend/src/Components/elements/Button.tsx
@@ -0,0 +1,24 @@
+import { Button as HButton } from "@heroui/react";
+import type { ButtonProps as HButtonProps } from "@heroui/react";
+import { useMemo } from "react";
+
+type ButtonProps = {
+  children: React.ReactNode;
+} & HButtonProps;
+
+export function Button({ children, ...props }: ButtonProps) {
+  const style = useMemo(() => {
+    switch (props.variant) {
+      case "solid":
+        return "bg-primary text-white font-bold";
+      default:
+        return "";
+    }
+  }, [props.variant]);
+
+  return (
+    <HButton {...props} className={`${style} ${props.className}`}>
+      {children}
+    </HButton>
+  );
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#48DE80",
+      },
+    },
   },
   plugins: [heroui()],
 };
