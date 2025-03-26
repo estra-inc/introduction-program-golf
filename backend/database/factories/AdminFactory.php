d3c0826751fbc142513e63cb26c0df2bc90c583e
motoki_yuto <motoki.youto293@gmail.com>
2025-03-26 19:51:34 +0900

    feat: me test作成

---
+++ backend/database/factories/AdminFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Factories;
+
+use App\Models\Admin;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AdminFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Admin::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->safeEmail(),
+            'password' => bcrypt('password'),
+        ];
+    }
+}
