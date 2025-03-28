fa67d9b5bed3608d690701794087ae0b7fd1200b
motoki_yuto <motoki.youto293@gmail.com>
2025-03-28 18:31:17 +0900

    fix: リクエスト型整理

--- frontend/src/lib/fetch.ts
+++ frontend/src/lib/fetch.ts
@@ -6,83 +6,76 @@ import {
 import { redirect } from "next/navigation";
 
 const isServerSide = typeof window === "undefined";
-async function fetchCsrfToken() {
-  await fetch(`${NEXT_PUBLIC_API_BASE_URL}/sanctum/csrf-cookie`, {
-    method: "GET",
-    credentials: "include",
-    cache: "no-store",
-  });
-}
 
-type BaseHttpOptions<HttpResponse = unknown> = {
-  method: string;
-  body?: Record<string, unknown>;
-  query?: Record<string, unknown>;
-  headers?: Record<string, string>;
+export type HttpOptions<HttpResponse> = {
   callbacks?: {
-    onSuccess?: (data: HttpResponse | null) => void;
+    onSuccess?: (data: HttpResponse) => void;
     onError?: (error: Error) => void;
     onAuthError?: () => void;
   };
 };
 
-export type HttpOptions = Omit<BaseHttpOptions, "method">;
+export type HttpParams<
+  PathParams = Record<string, string>,
+  QueryParams = Record<string, unknown>,
+  RequestBody = Record<string, unknown>
+> = {
+  pathParams?: PathParams;
+  queryParams?: QueryParams;
+  requestBody?: RequestBody;
+};
 
-export async function http<HttpResponse = null>(
-  url: string,
-  options: BaseHttpOptions<Response>
-): Promise<HttpResponse> {
-  const { onSuccess, onError, onAuthError } = options.callbacks || {};
+export type HttpDocument<ResponseBody = null> = {
+  params?: HttpParams;
+  options?: HttpOptions<ResponseBody>;
+};
 
-  const baseUrl = isServerSide
-    ? API_BASE_URL || NEXT_PUBLIC_API_BASE_URL
-    : NEXT_PUBLIC_API_BASE_URL;
-  const endpoint = `${baseUrl}${url}`;
+export async function http<T extends HttpDocument<R>, R = null>(
+  path: string,
+  method: string = "GET",
+  params?: T["params"],
+  options?: T["options"]
+): Promise<R> {
+  const { pathParams, queryParams, requestBody } = params ?? {};
+  const { onSuccess, onError, onAuthError } = options?.callbacks ?? {};
 
-  options.headers = {
+  const headers: RequestInit["headers"] = {
     Accept: "application/json",
     "Content-Type": "application/json",
   };
 
   if (isServerSide) {
     const nextHeaders = await import("next/headers");
-    const headers = await nextHeaders.headers();
-    options.headers = {
-      origin: NEXT_PUBLIC_FRONTEND_URL || "",
-      cookie: headers.get("cookie") ?? "",
-      ...options.headers,
-    };
+    const nextHeadersInstance = await nextHeaders.headers();
+    headers["origin"] = NEXT_PUBLIC_FRONTEND_URL || "";
+    headers["cookie"] = nextHeadersInstance.get("cookie") ?? "";
   }
 
-  if (["POST", "PUT", "PATCH", "DELETE"].includes(options.method || "GET")) {
+  if (["POST", "PUT", "PATCH", "DELETE"].includes(method)) {
     await fetchCsrfToken();
 
     if (isServerSide) {
       const nextHeaders = await import("next/headers");
       const cookies = await nextHeaders.cookies();
-      options.headers = {
-        "X-XSRF-TOKEN": cookies.get("XSRF-TOKEN")?.value ?? "",
-        ...options.headers,
-      };
+      headers["X-XSRF-TOKEN"] = cookies.get("XSRF-TOKEN")?.value ?? "";
     } else {
-      options.headers = {
-        "X-XSRF-TOKEN": decodeURIComponent(
+      headers["X-XSRF-TOKEN"] = decodeURIComponent(
+        decodeURIComponent(
           document.cookie
             .split("; ")
             .find((row) => row.startsWith("XSRF-TOKEN="))
             ?.split("=")[1] || ""
-        ),
-        ...options.headers,
-      };
+        )
+      );
     }
   }
 
-  const query = getQueryString(options);
-  const response = await fetch(endpoint + query, {
+  const endpoint = makeRequestUrl(path, pathParams ?? {}, queryParams ?? {});
+  const response = await fetch(endpoint, {
+    method: method,
+    headers,
+    body: requestBody ? JSON.stringify(requestBody) : undefined,
     credentials: "include",
-    headers: options.headers,
-    method: options.method,
-    body: options.body ? JSON.stringify(options.body) : undefined,
   });
 
   if (!response.ok) {
@@ -114,8 +107,8 @@ export async function http<HttpResponse = null>(
     }
   } else {
     if (response.status === 204) {
-      onSuccess?.(null);
-      return null as HttpResponse;
+      onSuccess?.(null as R);
+      return null as R;
     }
 
     try {
@@ -127,12 +120,74 @@ export async function http<HttpResponse = null>(
       return data;
     } catch (error) {
       console.log("Invalid JSON response", error);
-      return null as HttpResponse;
+      return null as R;
     }
   }
 
-  return null as HttpResponse;
+  return null as R;
 }
+
+/**
+ * CSRFトークンを取得する
+ */
+async function fetchCsrfToken() {
+  await fetch(`${NEXT_PUBLIC_API_BASE_URL}/sanctum/csrf-cookie`, {
+    method: "GET",
+    credentials: "include",
+    cache: "no-store",
+  });
+}
+
+/**
+ * API実行パスのプレースホルダを実際の値に置換して返却する
+ *
+ * @template T - HttpDocumentの型
+ * @param path - API実行パス
+ * @param pathParams - パスパラメータのオブジェクト
+ * @param queryParams - クエリパラメータのオブジェクト
+ * @returns パラメータ置換後のAPI実行パスの文字列
+ */
+function makeRequestUrl(
+  path: string,
+  pathParams: Record<string, string>,
+  queryParams: Record<string, unknown>
+) {
+  const baseUrl = isServerSide
+    ? API_BASE_URL || NEXT_PUBLIC_API_BASE_URL
+    : NEXT_PUBLIC_API_BASE_URL;
+
+  let endpoint = `${baseUrl}${path}`;
+
+  // パスパラメータが存在する場合, パスパラメータを置換
+  if (Object.keys(pathParams ?? {}).length > 0) {
+    endpoint = replacePathParams(endpoint, pathParams);
+  }
+
+  const query = getQueryString(queryParams);
+  return `${endpoint}${query}`;
+}
+
+/**
+ * API実行パスのプレースホルダを実際の値に置換して返却する
+ *
+ * @template PathParams - パスパラメータの型
+ * @param path - API実行パス
+ * @param pathParams - パスパラメータのオブジェクト
+ * @returns パラメータ置換後のAPI実行パスの文字列
+ */
+const replacePathParams = <PathParams extends Record<string, string>>(
+  path: string,
+  pathParams: PathParams
+) => {
+  return path.replace(/:([a-zA-Z0-9_]+)/g, (_, key) => {
+    const value = pathParams[key];
+    if (value === undefined) {
+      throw new Error(`Missing required path parameter: ${key}`);
+    }
+
+    return encodeURIComponent(value);
+  });
+};
 
 async function handleError() {
   const { addToast } = await import("@heroui/react");
@@ -143,16 +198,13 @@ async function handleError() {
   });
 }
 
-function getQueryString(options: Pick<HttpOptions, "query">): string {
-  const params = Object.entries(options.query || {}).reduce(
-    (acc, [key, value]) => {
-      if (value !== undefined) {
-        acc[key] = String(value);
-      }
-      return acc;
-    },
-    {} as Record<string, string>
-  );
+function getQueryString(options: Record<string, unknown>): string {
+  const params = Object.entries(options || {}).reduce((acc, [key, value]) => {
+    if (value !== undefined) {
+      acc[key] = String(value);
+    }
+    return acc;
+  }, {} as Record<string, string>);
 
   return params ? `?${new URLSearchParams(params).toString()}` : "";
 }
