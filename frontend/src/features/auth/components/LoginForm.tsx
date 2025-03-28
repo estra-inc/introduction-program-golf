27ae262c77c60fde85150bb8a8cf2dcd588e96a9
motoki_yuto <motoki.youto293@gmail.com>
2025-03-28 11:01:57 +0900

    fix: ログインフォーム useForm利用

--- frontend/src/features/auth/components/LoginForm.tsx
+++ frontend/src/features/auth/components/LoginForm.tsx
@@ -2,27 +2,62 @@
 
 import { Button } from "@/components/elements/Button";
 import { Input } from "@/components/elements/form";
+import { useForm } from "react-hook-form";
+import yup from "@/lib/yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import login from "./api/login";
+
+const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email("メールアドレス形式で入力してください。")
+    .required("メールアドレスは必須です。"),
+  password: yup.string().required("パスワードは必須です。"),
+});
+
+type LoginFormData = yup.InferType<typeof loginSchema>;
 
 type LoginFormProps = {
   onSuccess: () => void;
 };
 
 export default function LoginForm({ onSuccess }: LoginFormProps) {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    onSuccess();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: yupResolver(loginSchema),
+  });
+
+  const onSubmit = (data: LoginFormData) => {
+    login(data.email, data.password).then(() => {
+      onSuccess();
+    });
   };
 
   return (
     <form
       className="flex flex-col gap-10 w-96 text-center"
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <h1 className="text-4xl font-bold">ログイン</h1>
 
       <div className="flex flex-col gap-4">
-        <Input label="メールアドレス" variant="bordered" />
-        <Input label="パスワード" variant="bordered" />
+        <Input
+          label="メールアドレス"
+          variant="bordered"
+          {...register("email")}
+          errorMessage={errors.email?.message}
+          isInvalid={!!errors.email?.message}
+        />
+        <Input
+          label="パスワード"
+          variant="bordered"
+          {...register("password")}
+          errorMessage={errors.password?.message}
+          isInvalid={!!errors.password?.message}
+        />
       </div>
 
       <Button type="submit">ログイン</Button>
