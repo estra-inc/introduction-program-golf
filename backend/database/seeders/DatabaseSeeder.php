064cbd7f8d0d763dc0fcc5b92b85c7b6b2991fe4
motoki_yuto <motoki.youto293@gmail.com>
2025-03-31 16:09:59 +0900

    feat: Seeder作成

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -10,10 +10,9 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Admin::create([
-            'name' => 'admin',
-            'email' => 'admin@example.com',
-            'password' => Hash::make('password'),
+        $this->call([
+            AdminTableSeeder::class,
+            ReserveTableSeeder::class,
         ]);
     }
 }
