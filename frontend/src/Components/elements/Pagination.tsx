863203b2f49ee457ad4e8239489c3a54d61852d1
motoki_yuto <motoki.youto293@gmail.com>
2025-05-07 14:00:11 +0900

    Componentsディレクトリ追跡から外す

--- frontend/src/Components/elements/Pagination.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Pagination as HPagination } from "@heroui/react";
-
-import type { PaginationProps } from "@heroui/react";
-
-export function Pagination({ ...props }: PaginationProps) {
-  return <HPagination {...props} />;
-}
